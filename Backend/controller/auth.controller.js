5da0a07858de5a14dc9404adfee0196a6dadc827
as1265513 <60047565+as1265513@users.noreply.github.com>
2021-11-29 21:33:59 +0500

    create Organization
    Two things are required

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -1,6 +1,6 @@
 const config = require("../config/auth.config");
 const db = require("../model");
-const User = db.user;
+const {User} = db.user;
 const Role = db.role;
 
 var jwt = require("jsonwebtoken");
