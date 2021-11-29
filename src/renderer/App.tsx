a3c4991e1abfc3a8ee3fc0c4a820b1af500a35af
Rehan-05 <rehangoraya05.com>
2021-11-29 16:20:57 +0500

    changes

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { MemoryRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  MemoryRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import { AUTH } from 'Types/User.types';
 import './App.css';
 import Auth from './View/Authentication/Auth';
 import CreateProject from './View/createProject/createProject';
 require('react-web-vector-icons/fonts');
 
-function AuthRoute({children, ...rest}:any) {
-
-  const user = useSelector(({auth}:AUTH) => auth.user)
+function AuthRoute({ children, ...rest }: any) {
+  const user = useSelector(({ auth }: AUTH) => auth.user);
   const onlyChild = React.Children.only(children);
 
   return (
     <Route
       {...rest}
-      render={props =>
-          user ?
-            React.cloneElement(onlyChild, {...rest, ...props}) :
-            <Redirect to="/" />
+      render={(props) =>
+        user ? (
+          React.cloneElement(onlyChild, { ...rest, ...props })
+        ) : (
+          <Redirect to="/" />
+        )
       }
     />
-  )
+  );
 }
 export default function App() {
   return (
     <Router>
       <Switch>
-      <Route path="/" exact>
-        <Auth />
-      </Route>
-      <AuthRoute path="/createProject" >
-        <CreateProject />
-      </AuthRoute>
+        <Route path="/" exact>
+          <Auth />
+        </Route>
+        <AuthRoute path="/createProject">
+          <CreateProject />
+        </AuthRoute>
       </Switch>
     </Router>
   );
--- src/renderer/View/Authentication/Auth.tsx
+++ src/renderer/View/Authentication/Auth.tsx
@@ -1,54 +1,57 @@
 // import path from 'path';
 import { Col, Row } from 'react-bootstrap';
 
-import { Route,MemoryRouter as Router,Switch, Redirect } from 'react-router-dom'
-import { AppLogo,AuthBackPic } from '../../Constant/Images';
+import {
+  Route,
+  MemoryRouter as Router,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import { AppLogo, AuthBackPic } from '../../Constant/Images';
 import { Register, SignIn, ForgotPassword } from '.';
 import React from 'react';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import { AUTH } from 'Types/User.types';
 import Loader from 'renderer/Components/Loader';
 
-
-
 export default function Auth() {
-  const user=useSelector(({auth}:AUTH)=>{ return auth.user})
-  const isChecking=useSelector(({auth}:AUTH)=>{return (auth.login?.isChecking||auth.register?.isChecking)})
+  const user = useSelector(({ auth }: AUTH) => {
+    return auth.user;
+  });
+  const isChecking = useSelector(({ auth }: AUTH) => {
+    return auth.login?.isChecking || auth.register?.isChecking;
+  });
 
-  React.useEffect(()=>{
-
-
-    console.log("error" +isChecking)
-
-  },[isChecking])
-  if(isChecking) {
-    return (<div className="Container">
-      <Loader />
-    </div>)
+  React.useEffect(() => {
+    console.log('error' + isChecking);
+  }, [isChecking]);
+  if (isChecking) {
+    return (
+      <div className="Container">
+        <Loader />
+      </div>
+    );
   }
 
-  if(user)
-  {
-    return (<Redirect to="/createProject" />)
+  if (user) {
+    return <Redirect to="/createProject" />;
   }
   return (
     <div className="Container">
-
       <Row className="row">
         {/* Img Tag */}
-        <Col  className="col-5 col1 LeftSide"  >
+        <Col className="col-5 col1 LeftSide">
           <img src={AppLogo} className="AppLogoAuth" />
-          <img className="LeftSidePic" src={AuthBackPic}  />
+          <img className="LeftSidePic" src={AuthBackPic} />
           <div className="copyRightText">&copy;2021, Made by Dream Lab. </div>
         </Col>
         {/* Auth Routing  */}
-        <Col  className="col col1 right-col"  >
+        <Col className="col col1 right-col">
           <Router>
             <Switch>
-              <Route component={SignIn} path="/" exact  />
-              <Route component={Register} path="/Register"  />
+              <Route component={SignIn} path="/" exact />
+              <Route component={Register} path="/Register" />
               <Route component={ForgotPassword} path="/ForgotPassword" />
-
             </Switch>
           </Router>
         </Col>
