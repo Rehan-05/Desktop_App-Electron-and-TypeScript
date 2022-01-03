3c3adbce4fede5a6edd037e8bb8041de87e8fa72
as1265513 <60047565+as1265513@users.noreply.github.com>
2022-01-03 20:49:05 +0500

    a

--- src/renderer/View/Dashboard/Board.js
+++ src/renderer/View/Dashboard/Board.js
@@ -6,7 +6,7 @@ const data = {
       title: 'Planned Tasks',
       label: '2/2',
       cards: [
-        {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false},
+        {id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins'},
         {id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: {sha: 'be312a1'}}
       ]
     },
