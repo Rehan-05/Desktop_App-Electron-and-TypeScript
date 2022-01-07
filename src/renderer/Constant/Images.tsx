9f83805cf3e1bd79bf630043972e6cdc0dd0bd48
Rehan-05 <rehangoraya05.com>
2022-01-07 16:48:08 +0500

    New Update

--- src/renderer/Constant/Images.tsx
+++ src/renderer/Constant/Images.tsx
@@ -2,10 +2,12 @@ import AuthBackPic from '../../../assets/Programing.png';
 import AppLogo from '../../../assets/PA.png';
 import Mark from '../../../assets/mark.png';
 import line from '../../../assets/line.png';
+import Profile from '../../../assets/Profile.png';
 
 export{
   AuthBackPic,
   AppLogo,
   Mark,
-  line
+  line,
+  Profile
 }
